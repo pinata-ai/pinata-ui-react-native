d97f5ee26550fe2a5f756f9c2cac998e3abb6b1b
aidar <aidarionov@gmail.com>
2025-06-03 17:50:23 +0600

    feat: add text param in Wallet widget [PAR-1027]

--- src/components/Points/PinataPointsWallet.tsx
+++ src/components/Points/PinataPointsWallet.tsx
@@ -3,18 +3,20 @@ import BasePinataWebView from '../BasePinataWebView';
 interface PinataPointsWalletProps {
   token: string;
   onClick?: () => void;
+  text?: string;
 }
 
 export const PinataPointsWallet = ({
   token,
   onClick,
+  text,
 }: PinataPointsWalletProps) => {
   return (
     <BasePinataWebView
       onClick={onClick}
       name="pinata-points-wallet"
       getHtml={() => `
-                <pinata-points-wallet usertoken="${token}" style="font-size:clamp(0.8rem, 0.625rem + 0.875vw, 2.2rem);"></pinata-points-wallet>
+                <pinata-points-wallet usertoken="${token}" customCTAText=${text || 'See rewards'}></pinata-points-wallet>
             `}
     />
   );
